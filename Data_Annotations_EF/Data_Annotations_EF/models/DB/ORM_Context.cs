0c76b456a1ab35cca2158be816d7664326e88467
Badrillox <micperktold@tsn.at>
2022-11-09 11:25:23 +0100

    adding fluent-api

--- Data_Annotations_EF/Data_Annotations_EF/models/DB/ORM_Context.cs
+++ Data_Annotations_EF/Data_Annotations_EF/models/DB/ORM_Context.cs
@@ -4,11 +4,23 @@ namespace Data_Annotations_EF.models.DB;
 
 public class ORM_Context : DbContext
     {
+        public virtual DbSet<Evaluation> Evaluations { get; set; }
 
-    // DbSet<Klassenname> ermöglicht den Zugriff auf die DB-Tabelle Customers
-    public virtual DbSet<Evaluation> Evaluations { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Evaluation>()
+                .Property(eid => eid.EvaluationId)
+                .HasColumnName("Id")
+                .HasDefaultValue(0)
+                .IsRequired();
+            modelBuilder.Entity<Evaluation>()
+                .Property(t => t.Evaluation_Text)
+                .HasColumnName("Evaluation_Text")
+                .HasDefaultValue(null)
+                .IsRequired();
+        }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         const string constring = "Server=localhost;database=orm_test_5a;user=root;password=SHW_Destroyer02";
         optionsBuilder.UseSqlServer(constring);
