62b535e08a1b40f49a0207746e2e8e44312391c9
DreamTecHunter <Tobias.laser@ymail.com>
2022-11-16 11:12:24 +0100

    ausprogrammiert car; erstellt Additional und Carbill

---
+++ Autovermietung/Autovermietung/Autovermietung/models/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Annotations_EF.models
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public bool HasAirCondition { get; set; }
+        public int AmountSeats { get; set; }
+        public int SpaceSuitcases { get; set; }
+
+        //virtual List<CarBill> CarBills;
+
+        public Car(string type, string brand, string model, decimal price, bool hasAirCondition, int amountSeats, int SpaceSuitcases)
+        {
+            this.Type = type;
+            this.Brand = brand;
+            this.Model = model;
+            this.Price = price;
+            this.HasAirCondition = hasAirCondition;
+            this.AmountSeats = amountSeats;
+            this.SpaceSuitcases = SpaceSuitcases;
+        }
+
+        public Car() : this("none-type", "none-brand", "none-model", 0.0m, false, 4, 1)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return this.CarId + "\t" + this.Type + "\t" + this.Brand + "\t" + this.Model + "\t" + this.Price + "\t" + this.HasAirCondition + "\t" + this.AmountSeats + "\t" + this.SpaceSuitcases;
+        }
+    }
+}
