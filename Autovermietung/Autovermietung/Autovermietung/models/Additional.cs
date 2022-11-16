b7f87568ffef42ecf9948d5873c0d5ee265e6691
Badrillox <micperktold@tsn.at>
2022-11-16 11:26:21 +0100

    adding additionals

--- Autovermietung/Autovermietung/Autovermietung/models/Additional.cs
+++ Autovermietung/Autovermietung/Autovermietung/models/Additional.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Autovermietung.models
 {
-    internal class Additional
+    public class Additional
     {
+        public int Id { get; set; }
+        public bool ChildSeat { get; set; }
+        public bool Navigation { get; set; }
+        public bool SkiRack { get; set; }
+        public bool SnowChains { get; set; }
+        public bool RoofRack { get; set; }
+        public bool BikeRack { get; set; }
+
+        public Additional() : this(false, false, false, false, false, false) { }
+
+        public Additional(bool cs, bool navi, bool sr, bool sc, bool rr, bool br)
+        {
+            ChildSeat = cs; Navigation = navi; SkiRack = sr; SnowChains = sc; RoofRack = rr; BikeRack = br;
+        }
+        public override string ToString()
+        {
+            return $"ChildSeat: {ChildSeat}, Navigation: {Navigation}, SkiRack: {SkiRack}, SnowChains: {SnowChains}, " +
+                $"RoofRack: {RoofRack}, BikeRack: {BikeRack}";
+        }
     }
 }
--- Autovermietung/Autovermietung/Autovermietung/models/DB/DBManager.cs
+++ Autovermietung/Autovermietung/Autovermietung/models/DB/DBManager.cs
@@ -12,7 +12,9 @@ namespace Autovermietung.models.DB
         public ConstringManager cm = new ConstringManager();
         public virtual DbSet<Customer> Customers { get; set; }
         public virtual DbSet<Bills> Bills { get; set; }
-        //public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<CarBill> CarBills { get; set; }
+        public virtual DbSet<Additional> Additionals { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = $"Server={cm.Server};database={cm.Database};user={cm.User};password={cm.Pw}";
