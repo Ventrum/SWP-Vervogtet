691f3707649bcc1e2eb2f24bf6a57ad303eeb375
Badrillox <micperktold@tsn.at>
2022-11-16 10:45:45 +0100

    pushing bills and customer

---
+++ Autovermietung/Autovermietung/Autovermietung/models/Bills.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autovermietung.models
+{
+    public class Bills
+    {
+        public int BillID { get; set; }
+        public int CarID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double TotalPrice { get; set; }
+
+        public Customer Customer { get; set; }
+
+        // ctors
+        public Bills() : this(0, 0, DateTime.Now, DateTime.Now, 0.0, null)
+        {
+            // default ctor
+        }
+
+        public Bills(int billid, int carid, DateTime startdate, 
+            DateTime enddate, double totalprice, Customer customer)
+        {
+            BillID = billid; CarID = carid; 
+            StartDate = startdate; EndDate = enddate; TotalPrice = totalprice;
+            Customer = customer;
+        }
+        public override string ToString()
+        {
+            return $"{BillID} {CarID} \n{StartDate} - {EndDate} \n {TotalPrice} \n{Customer}";
+        }
+    }
+}
---
+++ Autovermietung/Autovermietung/Autovermietung/models/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autovermietung.models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string? StreetNr { get; set; } // kann null sein, wenn keine weitere Adresse vorhanden
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public List<Bills> Bills { get; set; }
+        // ctors
+        public Customer() : this("", "", "", "", "", "", "", "", "", DateTime.Now, null)
+        {
+            // default ctor
+        }
+        
+        public Customer(string firstname, string lastname, string street, string? streetrn, 
+            string zipcode, string city, string country, string phonenr,
+            string email, DateTime bday, List<Bills> bills)
+        {
+            FirstName = firstname; LastName = lastname; Street = street; StreetNr = streetrn;
+            ZipCode = zipcode; City = city; Country = country; Phone = phonenr; Email = email;
+            BirthDate = bday; Bills = bills;
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}" +
+                $"{BirthDate}" + 
+                $"{((StreetNr != null) ? $" {StreetNr}" : "") }"
+            + $", {ZipCode} {City}, {Country}" +
+            $"{Phone} {Email} \n{Bills}"
+            ;
+        }
+    }
+}
