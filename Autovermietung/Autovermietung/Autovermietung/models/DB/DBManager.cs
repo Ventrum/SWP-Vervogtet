ed738939b13ddb9605e27b780e09c1cc8d912c8e
DreamTecHunter <Tobias.laser@ymail.com>
2022-12-13 21:25:27 +0100

    commited all relevant files to git, except DBManager.

--- Autovermietung/Autovermietung/Autovermietung/models/DB/DBManager.cs
+++ Autovermietung/Autovermietung/Autovermietung/models/DB/DBManager.cs
@@ -9,13 +9,15 @@ namespace Autovermietung.models.DB
 {
     public class DBManager : DbContext
     {
-        public ConstringManager cm = new ConstringManager();
+        //public ConstringManager cm = new ConstringManager();
         public virtual DbSet<Customer> Customers { get; set; }
-        public virtual DbSet<Bills> Bills { get; set; }
-        //public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<Additional> Additionals { get; set; }
+        public virtual DbSet<Bill> Bills { get; set; }
+        public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<CarBill> CarsBill { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string connectionString = $"Server={cm.Server};database={cm.Database};user={cm.User};password={cm.Pw}";
+            string connectionString = $"Server=localhost;database=autovermietung;user=swp-vogt;password=swp-vogt";
             optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         }
     }
